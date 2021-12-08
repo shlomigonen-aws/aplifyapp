1f2d5b714e746a0df5f111ecfcd95ac6540455b5
shlomigonen-aws <72110974+shlomigonen-aws@users.noreply.github.com>
2021-12-08 15:13:26 -0800

    Update App.js
    fixing html

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <br>
-          <br>
           Welcome to SMD Client Web Application Data Download Tool (SCWADD) 
         </p>
         <a
