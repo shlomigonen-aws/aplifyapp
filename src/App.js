b7bde63d53489d19dedcafeda0d7ee0e0c63daea
shlomigonen-aws <72110974+shlomigonen-aws@users.noreply.github.com>
2021-12-08 15:18:24 -0800

    Update App.js
    attempt to remove the spinning logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>
           Welcome to SMD Client Web Application Data Download Tool (SCWADD) 
         </p>
