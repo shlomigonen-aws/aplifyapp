7aff2ca4303c7990d89de8504525f8643d006d04
shlomigonen-aws <72110974+shlomigonen-aws@users.noreply.github.com>
2021-12-08 15:30:48 -0800

    Update App.js
    larger image

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" height="200"/>
+        <img src={logo} alt="logo" height="300"/>
         <p>
           Welcome to SMD Client Web Application Data Download Tool (SCWADD) 
         </p>
